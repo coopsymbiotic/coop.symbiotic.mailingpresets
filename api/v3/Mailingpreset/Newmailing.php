b4650d471978c9e9155e8a1eb98a847b6d4ad4a8
Mathieu Lutfy <mathieu@bidon.ca>
2015-11-22 16:10:48 -0500

    Make it possible to embed this extension from a third-party site, in order to create a new mailing using the content of a website.

---
+++ api/v3/Mailingpreset/Newmailing.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Mailingpreset.newmailing API
+ *
+ * @param array $params
+ * @return array API result descriptor
+ * @throws API_Exception
+ */
+function civicrm_api3_mailingpreset_newmailing($params) {
+  $result = array();
+
+  $dao = CRM_Core_DAO::executeQuery('SELECT * FROM civicrm_mailingpreset WHERE id = %1', array(
+    1 => array($params['id'], 'Positive'),
+  ));
+
+  if ($dao->fetch()) {
+    // Fetch body template
+    $tpl = civicrm_api3('MessageTemplate', 'getsingle', array(
+      'id' => $dao->template_id,
+    ));
+
+    // Inject content in template body
+    $msg_html = preg_replace('/<!--MAILINGCONTENT-->/', $params['body'], $tpl['msg_html']);
+
+    $res = civicrm_api3('Mailing', 'create', array(
+      'name' => $dao->name,
+      'subject' => $dao->subject,
+      'body_html' => $msg_html,
+    ));
+
+    // Inject the CiviMail URL into the result, so we that we can redirect here.
+    $res['url'] = CRM_Utils_System::url('civicrm/mailing/send', array('mid' => $res['id'], 'continue' => 'true', 'reset' => 1), TRUE, FALSE, TRUE);
+
+    return $res;
+  }
+
+  return $result;
+}
---
+++ mailingpresets.js
@@ -0,0 +1,69 @@
+jQuery(function ($) {
+  $('#civicrm-mailingpreset > a').click(function(event) {
+    var $button = $(this);
+
+    // Expect an element sur as:
+    // <a href="#" data-baseurl="https://mycivi.example.org/" data-mailingpreset="1" data-selector=".main-container">Create mailing</a>
+    var baseurl = $button.data('baseurl');
+    var mailingpreset = $button.data('mailingpreset');
+    var selector = $button.data('selector');
+
+    $.ajaxPrefilter(function(options, originalOptions, jqXHR) {
+      options.crossDomain = { crossDomain: true };
+      options.xhrFields = { withCredentials: true };
+    });
+
+    if (! selector) {
+      selector = 'body';
+    }
+
+    if ($(selector).size() != 1) {
+      alert("Invalid configuration. Please provide an HTML selector that matches only one element.");
+      event.preventDefault();
+      return;
+    }
+
+    // Spin the button background, to show work in progress.
+    $button.prop('disabled', true);
+    var button_id = $(this).attr('id');
+
+    $button.addClass('crm-spinning');
+
+    // Fetch page body and remove tags that clearly should not be in the mailing.
+    var $html = $(selector).clone();
+    $html.find('script').remove();
+    $html.find('link').remove();
+
+    var body_html = $html.html();
+
+    // Call our special API method that will create a new mailing
+    // using the configurations from our preset.
+    var ajax = $.ajax({
+      url: baseurl + 'civicrm/ajax/rest',
+      dataType: 'json',
+      data: {
+        entity: 'Mailingpreset',
+        action: 'newmailing',
+        json: JSON.stringify({ id: mailingpreset, body: body_html })
+      },
+      xhrFields: { withCredentials: true },
+      headers: {'X-Requested-With': 'XMLHttpRequest'},
+      beforeSend: function(jqXHR, settings) {
+        jqXHR.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+      },
+      type: 'POST',
+      error: function(request, status, error) {
+        alert("Access denied. Please make sure you are authenticated in your CiviCRM site. If you are using Chrome or Safari, make sure you are not blocking third-party cookies in your Settings → Advanced settings → Content settings.");
+      }
+    })
+    .done(function(data) {
+      // Redirect to civimail
+      window.location.href = data.url;
+
+      $button.removeClass('crm-spinning');
+      $button.append('<span> ✔</span>');
+    });
+
+    event.preventDefault();
+  });
+});
